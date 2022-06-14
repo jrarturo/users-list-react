85d94d5c194892ee30fa537aa37869c5ad3922d2
jrarturo <62453825+jrarturo@users.noreply.github.com>
2022-06-13 20:39:49 -0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import '././index.css';
+
+const app = <img src='/logo.svg' />;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(app);
