6e15653b9a140faf38009bad8ad29d604465a28b
Alvaro Arturo <alvaroj.arturo@gmail.com>
2022-06-23 01:35:18 -0500

    clean code modular

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,13 @@ import UserList from './components/UserList';
 const USERS = [
 	{ name: 'John Doe Fee', active: true, role: 'admin' },
 	{ name: 'Peter Grey Master', active: false, role: 'user' },
-	{ name: 'Mary Poppings T.', active: true, role: 'user' }
-	// { name: 'Jane Grey Foo', active: false, role: 'admin' },
-	// { name: 'Jack Black Doe', active: true, role: 'user' },
-	// { name: 'Jill Tatareto', active: false, role: 'user' },
-	// { name: 'Joe Gunanoe', active: true, role: 'addsdmin' }
+	{ name: 'Mary Poppings T.', active: true, role: 'user' },
+	{ name: 'Jane Grey Foo', active: false, role: 'admin' },
+	{ name: 'Jack Black Doe', active: true, role: 'user' },
+	{ name: 'Jill Tatareto', active: false, role: 'user' },
+	{ name: 'Joe Gunanoe', active: true, role: 'addsdmin' }
 ];
 
-const App = () => (
-	<UserList users={USERS}>
-		<h1>Users List</h1>
-	</UserList>
-);
+const App = () => <UserList users={USERS} />;
 
 export default App;
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,22 +1,42 @@
+import { useState } from 'react';
 import style from './UserList.module.css';
 import UserRow from './UserRow';
 
-const UserList = ({ users, children }) => {
-	console.log('    - UserList render');
+const UserList = ({ users }) => {
+	const [search, setSearch] = useState('');
+
+	const usersRendered = filterAndRenderUsers(users, search);
+
+	return (
+		<div className={style.list}>
+			<h1>Users List</h1>
+			<input
+				type='text'
+				name='search'
+				value={search}
+				onChange={ev => setSearch(ev.target.value)}
+			/>
+			<button type='submit'>Search</button>
+			{usersRendered}
+		</div>
+	);
+};
+
+const filterAndRenderUsers = (users, search) => {
+	const normalizeSearch = search.toLocaleLowerCase();
+
+	const filteredUsers = search
+		? users.filter(user => user.name.toLowerCase().startsWith(normalizeSearch))
+		: users;
 
 	const userRender =
-		users.length > 0 ? (
-			users.map(user => <UserRow key={user.name} {...user} />)
+		filteredUsers.length > 0 ? (
+			filteredUsers.map(user => <UserRow key={user.name} {...user} />)
 		) : (
 			<div className={style.noUsers}>No users to show</div>
 		);
 
-	return (
-		<div className={style.list}>
-			{children}
-			{userRender}
-		</div>
-	);
+	return userRender;
 };
 
 export default UserList;
